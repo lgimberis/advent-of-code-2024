6723c7995a9f6b3d86c6644a703ee8a3b1cbb205
Lukas Gimberis <lukasgimberis@gmail.com>
2024-12-01 18:29:33 +0000

    Add working 01p2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,8 @@
 use advent_of_code_2024::read_today_data_file;
 use regex::Regex;
+use std::collections::HashMap;
 
-fn part_one(content: &String) -> u32 {
+fn sort_input_arrays(content: &String) -> (Vec<u32>, Vec<u32>) {
     let mut first: Vec<u32> = Vec::new();
     let mut second: Vec<u32> = Vec::new();
     let re = Regex::new(r"^(?P<left>\d+)\s+(?P<right>\d+)$").unwrap();
@@ -23,6 +24,11 @@ fn part_one(content: &String) -> u32 {
     first.sort();
     second.sort();
 
+    (first, second)
+}
+
+fn part_one(content: &String) -> u32 {
+    let (first, second) = sort_input_arrays(content);
     let mut sum_of_differences: u32 = 0;
     for i in 0..first.len() {
         sum_of_differences += first[i].abs_diff(second[i]);
@@ -44,8 +50,48 @@ fn test_part_one() {
     assert_eq!(result, 11);
 }
 
+fn part_two(content: &String) -> u32 {
+    let (first, second) = sort_input_arrays(content);
+
+    // Construct hash maps of both arrays
+    let mut first_map = HashMap::new();
+    let mut second_map = HashMap::new();
+    for i in first {
+        let count = first_map.entry(i).or_insert(0);
+        *count += 1;
+    }
+    for i in second {
+        let count = second_map.entry(i).or_insert(0);
+        *count += 1;
+    }
+
+    let mut similarity_score = 0;
+    for (key, value) in &first_map {
+        if second_map.contains_key(key) {
+            similarity_score += *key * *value * *second_map.entry(*key).or_insert(0);
+        }
+    }
+
+    similarity_score
+}
+
+#[test]
+fn test_part_two() {
+    let test_data = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+
+    let result = part_two(&test_data.to_string());
+    assert_eq!(result, 31);
+}
+
 fn main() {
     let content = read_today_data_file("01".to_string());
     let sum_of_differences = part_one(&content);
-    println!("{sum_of_differences}");
+    println!("Part one: Sum of differences = {sum_of_differences}");
+    let similarity_score = part_two(&content);
+    println!("Part two: Similarity score = {similarity_score}");
 }
