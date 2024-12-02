5a5208ae0d542c3f7a23981366a61dbbe4a32b28
Lukas Gimberis <lukasgimberis@gmail.com>
2024-12-02 13:22:52 +0000

    Add 02p2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -18,7 +18,7 @@ fn parse_input(data: String) -> Vec<Vec<u32>> {
     reports
 }
 
-pub fn part_one(reports: Vec<Vec<u32>>) -> u32 {
+pub fn part_one(reports: &Vec<Vec<u32>>) -> u32 {
     let mut number_safe = 0;
     'outer: for report in reports {
         if report.len() < 1 {
@@ -42,6 +42,87 @@ pub fn part_one(reports: Vec<Vec<u32>>) -> u32 {
     number_safe
 }
 
+pub fn part_two(reports: &Vec<Vec<u32>>) -> u32 {
+    let mut number_safe = 0;
+    'outer: for report in reports {
+        if report.len() < 2 {
+            if report.len() == 1 {
+                number_safe += 1;
+            }
+            continue;
+        };
+        let direction_matters = report.len() > 3; // Direction does not matter when the report is
+                                                  // of length 3. We can remove any node and still have direction respected.
+
+        fn delta(report: &Vec<u32>, i: usize, problem_index: Option<usize>) -> i32 {
+            match problem_index {
+                Some(p) => {
+                    if i == p {
+                        return report[i + 1] as i32 - report[i - 1] as i32;
+                    } else if i - 1 == p {
+                        return report[i] as i32 - report[i - 2] as i32;
+                    } else {
+                        return report[i] as i32 - report[i - 1] as i32;
+                    }
+                }
+                None => {
+                    return report[i] as i32 - report[i - 1] as i32;
+                }
+            }
+        }
+        fn is_delta_increasing(report: &Vec<u32>, i: usize, problem_index: Option<usize>) -> bool {
+            return delta(report, i, problem_index) > 0;
+        }
+        fn is_delta_invalid(
+            report: &Vec<u32>,
+            i: usize,
+            direction: Option<bool>,
+            problem_index: Option<usize>,
+        ) -> bool {
+            let this_delta = delta(report, i, problem_index);
+            let direction_factor = match direction {
+                Some(d) => d == is_delta_increasing(report, i, problem_index),
+                None => true,
+            };
+            return !direction_factor || this_delta == 0 || this_delta.abs() > 3;
+        }
+
+        let mut is_increasing = false;
+        let mut problem_index: Option<usize> = None;
+        if direction_matters {
+            if is_delta_increasing(&report, 1, None) == is_delta_increasing(&report, 2, None) {
+                is_increasing = is_delta_increasing(&report, 1, None);
+            } else {
+                is_increasing = is_delta_increasing(&report, 3, None);
+            }
+        }
+        let mut direction_option = None;
+        if direction_matters {
+            direction_option = Some(is_increasing);
+        }
+        for i in 1..report.len() {
+            if is_delta_invalid(&report, i, direction_option, problem_index) {
+                if problem_index.is_some() {
+                    continue 'outer; // This report is invalid; second mistake
+                }
+                // Try right delta first as left is known to be valid
+                problem_index = Some(i);
+                if i + 1 < report.len()
+                    && is_delta_invalid(&report, i + 1, direction_option, problem_index)
+                {
+                    // Still invalid - try left
+                    problem_index = Some(i - 1);
+                    if i > 1 && is_delta_invalid(&report, i - 1, direction_option, problem_index) {
+                        continue 'outer; // Unfixable problem
+                    }
+                }
+            }
+        }
+        number_safe += 1;
+    }
+    number_safe
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -56,13 +137,51 @@ mod tests {
     #[test]
     fn test_part_one() {
         let input = parse_input(TEST_DATA.to_string());
-        let result = part_one(input);
+        let result = part_one(&input);
         assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn test_part_two_basic_remove_second() {
+        let input = parse_input(String::from("1 0 4 5"));
+        let result = part_two(&input);
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn test_part_two_basic_remove_third() {
+        let input = parse_input(String::from("3 2 6 1"));
+        let result = part_two(&input);
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn test_part_two_basic_remove_first() {
+        let input = parse_input(String::from("0 4 6 8"));
+        let result = part_two(&input);
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn test_part_two_basic_remove_last() {
+        let input = parse_input(String::from("1 2 3 4 5 6 1"));
+        let result = part_two(&input);
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn test_part_two_sample() {
+        let input = parse_input(TEST_DATA.to_string());
+        let result = part_two(&input);
+        assert_eq!(result, 4);
     }
 }
 
 fn main() {
     let file = read_today_data_file(String::from("02"));
-    let part_one_result = part_one(parse_input(file));
+    let parsed = parse_input(file);
+    let part_one_result = part_one(&parsed);
     println!("Part one output: {part_one_result}");
+    let part_two_result = part_two(&parsed);
+    println!("Part two output: {part_two_result}");
 }
