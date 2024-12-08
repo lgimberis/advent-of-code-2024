677a0ee49c63afa7a9f9ead619d0c1f4af25c2c1
Lukas Gimberis <lukasgimberis@gmail.com>
2024-12-08 12:31:25 +0000

    Add 08p2

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -22,25 +22,52 @@ fn find_antennas(map: Vec<Vec<char>>) -> HashMap<char, Vec<(i32, i32)>> {
     result
 }
 
-fn find_antinodes(a1: (i32, i32), a2: (i32, i32)) -> (Option<(i32, i32)>, Option<(i32, i32)>) {
+fn find_antinode_single(a1: (i32, i32), a2: (i32, i32), bounds: (i32, i32)) -> Vec<(i32, i32)> {
     // Core idea: When nodes are seperated by dx in x and dy in y, the antinodes are located at
     // (x1 - dx, y1 - dy) and (x2 + dx, y2 + dy)
+    let mut result = Vec::new();
     let dx = a2.0 - a1.0;
     let dy = a2.1 - a1.1;
-    let antinode1 = if a1.0 < dx || a1.1 < dy {
-        None
-    } else {
-        Some((a1.0 - dx, a1.1 - dy))
-    };
-    let antinode2 = if a2.0 < -dx || a1.1 < -dy {
-        None
-    } else {
-        Some((a2.0 + dx, a2.1 + dy))
-    };
-    (antinode1, antinode2)
+    if a1.0 - dx >= 0 && a1.1 - dy >= 0 && a1.0 - dx < bounds.0 && a1.1 - dy < bounds.1 {
+        result.push((a1.0 - dx, a1.1 - dy));
+    }
+    if a2.0 + dx >= 0 && a2.1 + dy >= 0 && a2.0 + dx < bounds.0 && a2.1 + dy < bounds.1 {
+        result.push((a2.0 + dx, a2.1 + dy));
+    }
+    result
 }
 
-fn part_one(file: &String) -> i64 {
+fn find_antinodes_multiple(a1: (i32, i32), a2: (i32, i32), bounds: (i32, i32)) -> Vec<(i32, i32)> {
+    // Core idea: When nodes are seperated by dx in x and dy in y, the antinodes are located at
+    // (x1 - dx, y1 - dy) and (x2 + dx, y2 + dy)
+    let mut result = Vec::new();
+    let dx = a2.0 - a1.0;
+    let dy = a2.1 - a1.1;
+    let mut i = 0;
+    while a1.0 - i * dx >= 0
+        && a1.1 - i * dy >= 0
+        && a1.0 - i * dx < bounds.0
+        && a1.1 - i * dy < bounds.1
+    {
+        result.push((a1.0 - i * dx, a1.1 - i * dy));
+        i += 1;
+    }
+    i = 0;
+    while a2.0 + i * dx >= 0
+        && a2.1 + i * dy >= 0
+        && a2.0 + i * dx < bounds.0
+        && a2.1 + i * dy < bounds.1
+    {
+        result.push((a2.0 + i * dx, a2.1 + i * dy));
+        i += 1;
+    }
+    result
+}
+
+fn either_part(
+    file: &String,
+    find_antinodes: fn((i32, i32), (i32, i32), (i32, i32)) -> Vec<(i32, i32)>,
+) -> i64 {
     let parsed_input = parse_input(file);
     let height = parsed_input.len();
     let width = parsed_input[0].len(); // Assumed constant
@@ -50,18 +77,8 @@ fn part_one(file: &String) -> i64 {
     for (_c, antenna_locations) in antenna_map {
         for (i, loc1) in antenna_locations.iter().enumerate() {
             for loc2 in &antenna_locations[i + 1..] {
-                let (antinode1, antinode2) = find_antinodes(*loc1, *loc2);
-                if antinode1.is_some() {
-                    let (x, y) = antinode1.unwrap();
-                    if x < width as i32 && y < height as i32 {
-                        map[x as usize][y as usize] = true;
-                    }
-                }
-                if antinode2.is_some() {
-                    let (x, y) = antinode2.unwrap();
-                    if x < width as i32 && y < height as i32 {
-                        map[x as usize][y as usize] = true;
-                    }
+                for (x, y) in find_antinodes(*loc1, *loc2, (width as i32, height as i32)) {
+                    map[x as usize][y as usize] = true;
                 }
             }
         }
@@ -78,9 +95,12 @@ fn part_one(file: &String) -> i64 {
     antinodes
 }
 
+fn part_one(file: &String) -> i64 {
+    either_part(file, find_antinode_single)
+}
+
 fn part_two(file: &String) -> i64 {
-    let parsed_input = parse_input(file);
-    0
+    either_part(file, find_antinodes_multiple)
 }
 
 #[cfg(test)]
@@ -109,7 +129,7 @@ mod tests {
     #[test]
     fn test_part_two_as_given() {
         let result = part_two(&String::from(EXAMPLE_DATA));
-        assert_eq!(result, -1);
+        assert_eq!(result, 34);
     }
 }
 
