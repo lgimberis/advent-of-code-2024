7cd4b3e0f18aaf9b50deb3ba2710ac444d215513
Lukas Gimberis <lukasgimberis@gmail.com>
2024-12-16 14:13:45 +0000

    Postmortem d16: use common-sense types such as usize instead of i32 to refer to variables relating to indices for example, using .checked_add_signed to avoid the spam of 'as' type casting

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -25,52 +25,69 @@ fn find_start_and_end(grid: &Vec<Vec<char>>) -> ((usize, usize), (usize, usize))
     (start, end)
 }
 
-const DIRECTIONS: [(i32, i32); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+const DIRECTIONS: [(isize, isize); 4] = [(1, 0), (0, 1), (-1, 0), (0, -1)];
 
-fn score_cost_turning(d: usize, other: usize) -> i32 {
-    1000 * std::cmp::min(other.abs_diff(4 - d), other.abs_diff(d)) as i32
+fn score_cost_turning(d: usize, other: usize) -> u64 {
+    1000 * std::cmp::min(other.abs_diff(4 - d), other.abs_diff(d)) as u64
 }
 
-fn generate_map(grid: Vec<Vec<char>>) -> HashMap<(i32, i32), Vec<(usize, i32)>> {
-    let width = grid[0].len() as i32;
-    let height = grid.len() as i32;
+fn generate_map(grid: Vec<Vec<char>>) -> HashMap<(usize, usize), Vec<(usize, u64)>> {
+    let width = grid[0].len();
+    let height = grid.len();
     let (start, _end) = find_start_and_end(&grid);
-    let mut unmapped_points = VecDeque::new();
-    unmapped_points.push_back(((start.0 as i32, start.1 as i32), 0, 0));
+
+    let mut tile_processing_queue = VecDeque::from([(start, 0, 0)]);
     let mut map = HashMap::new();
     let mut it = 0;
-    while unmapped_points.len() > 0 && it < 999_999 {
+
+    while tile_processing_queue.len() > 0 && it < 999_999 {
         it += 1;
 
-        let ((x, y), d, score) = unmapped_points.pop_front().unwrap();
+        let ((x, y), d, score) = tile_processing_queue.pop_front().unwrap();
 
-        for (i, (dx, dy)) in DIRECTIONS.iter().enumerate() {
-            if x + dx >= 0
-                && x + dx < width
-                && y + dy >= 0
-                && y + dy < height
-                && grid[(y + dy) as usize][(x + dx) as usize] != '#'
-            {
-                let v = map.get(&(x + dx, y + dy));
-                let new_score = score + 1 + score_cost_turning(i, d);
-                let mut new_v = vec![(i, new_score)];
-                if v.is_some() {
-                    let o: &Vec<(usize, i32)> = v.unwrap();
-                    if o.iter().any(|&(_d, s)| {
-                        s + score_cost_turning(d, _d) < new_score || (d == _d && s <= new_score)
-                    }) {
+        for (i, &(dx, dy)) in DIRECTIONS.iter().enumerate() {
+            let new_x = match x.checked_add_signed(dx) {
+                None => continue,
+                Some(_x) => {
+                    if _x < width {
+                        _x
+                    } else {
                         continue;
                     }
-                    let o2 = o
-                        .iter()
-                        .filter(|&(_d, s)| *s <= new_score + score_cost_turning(*_d, d))
-                        .map(|&s| s)
-                        .collect::<Vec<(usize, i32)>>();
-                    new_v.extend(o2);
                 }
-                map.insert((x + dx, y + dy), new_v);
-                unmapped_points.push_back(((x + dx, y + dy), i, new_score));
+            };
+            let new_y = match y.checked_add_signed(dy) {
+                None => continue,
+                Some(_y) => {
+                    if _y < height {
+                        _y
+                    } else {
+                        continue;
+                    }
+                }
+            };
+            if grid[new_y][new_x] == '#' {
+                continue;
             }
+            let v = map.get(&(new_x, new_y));
+            let new_score = score + 1 + score_cost_turning(i, d);
+            let mut new_v = vec![(i, new_score)];
+            if v.is_some() {
+                let o: &Vec<(usize, u64)> = v.unwrap();
+                if o.iter().any(|&(_d, s)| {
+                    s + score_cost_turning(d, _d) < new_score || (d == _d && s <= new_score)
+                }) {
+                    continue;
+                }
+                let o2 = o
+                    .iter()
+                    .filter(|&(_d, s)| *s <= new_score + score_cost_turning(*_d, d))
+                    .map(|&s| s)
+                    .collect::<Vec<(usize, u64)>>();
+                new_v.extend(o2);
+            }
+            map.insert((new_x, new_y), new_v);
+            tile_processing_queue.push_back(((new_x, new_y), i, new_score));
         }
     }
     if it == 999_999 {
@@ -79,14 +96,14 @@ fn generate_map(grid: Vec<Vec<char>>) -> HashMap<(i32, i32), Vec<(usize, i32)>> 
     map
 }
 
-fn part_one(file: &String) -> i64 {
+fn part_one(file: &String) -> u64 {
     let grid = parse_input(file);
     let (_start, end) = find_start_and_end(&grid);
     let map = generate_map(grid);
-    let mut min_end_score = i64::MAX;
-    let v = map.get(&(end.0 as i32, end.1 as i32));
+    let mut min_end_score = u64::MAX;
+    let v = map.get(&end);
     for (_d, score) in v.unwrap() {
-        min_end_score = std::cmp::min(*score as i64, min_end_score);
+        min_end_score = std::cmp::min(*score, min_end_score);
     }
     min_end_score
 }
@@ -99,11 +116,11 @@ fn part_two(file: &String) -> i64 {
     let mut best_path_points = HashSet::new();
     let mut path_points = VecDeque::new();
     let score = map
-        .get(&(end.0 as i32, end.1 as i32))
+        .get(&end)
         .unwrap()
         .iter()
-        .fold(i32::MAX, |acc, el| std::cmp::min(el.1, acc));
-    path_points.push_back(((end.0 as i32, end.1 as i32), score));
+        .fold(u64::MAX, |acc, el| std::cmp::min(el.1, acc));
+    path_points.push_back((end, score));
     while path_points.len() > 0 {
         let (point, score) = path_points.pop_front().unwrap();
         best_path_points.insert(point);
@@ -113,7 +130,16 @@ fn part_two(file: &String) -> i64 {
                 for (d, s) in v {
                     if *s <= score {
                         path_points.push_back((
-                            (point.0 - DIRECTIONS[*d].0, point.1 - DIRECTIONS[*d].1),
+                            (
+                                point
+                                    .0
+                                    .checked_add_signed(-DIRECTIONS[*d].0)
+                                    .expect("Overflow during backtracking"),
+                                point
+                                    .1
+                                    .checked_add_signed(-DIRECTIONS[*d].1)
+                                    .expect("Overflow during backtracking"),
+                            ),
                             *s,
                         ));
                     }
