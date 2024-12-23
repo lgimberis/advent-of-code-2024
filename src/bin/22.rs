fcab83e2f660bb7889b506d7741adb832d496ae7
Lukas Gimberis <lukasgimberis@gmail.com>
2024-12-23 13:36:41 +0000

    Add 22p2

--- src/bin/22.rs
+++ src/bin/22.rs
@@ -1,5 +1,6 @@
 use advent_of_code_2024::read_today_data_file;
 use itertools::Itertools;
+use std::collections::{HashMap, HashSet, VecDeque};
 
 fn parse_input(file: &String) -> Vec<u64> {
     file.lines()
@@ -18,7 +19,7 @@ fn next_secret(secret: u64) -> u64 {
 
 fn generate_nth_secret(secret: u64, n: u64) -> u64 {
     let mut s = secret;
-    for i in 0..n {
+    for _i in 0..n {
         s = next_secret(s);
     }
     s
@@ -32,9 +33,44 @@ fn part_one(file: &String) -> u64 {
         .sum()
 }
 
+fn generate_deltas(secret: u64, n: u64) -> Vec<(u64, i64)> {
+    let mut previous = secret;
+    let mut v = Vec::new();
+    for _i in 0..n {
+        let s = next_secret(previous);
+        let price = s % 10;
+        let previous_price = (previous % 10) as i64;
+        v.push((price % 10, price as i64 - previous_price));
+        previous = s;
+    }
+    v
+}
 fn part_two(file: &String) -> u64 {
-    let parsed_input = parse_input(file);
-    0
+    let secret_numbers = parse_input(file);
+    let mut sequence_earnings = HashMap::new();
+    for secret in secret_numbers {
+        let deltas = generate_deltas(secret, 2000);
+        let mut queue = VecDeque::new();
+        let mut seen = HashSet::new();
+        for (price, delta) in deltas {
+            queue.push_back(delta);
+            if queue.len() < 4 {
+                continue;
+            }
+            let key = queue[0] * 8000 + queue[1] * 400 + queue[2] * 20 + queue[3];
+            if seen.contains(&key) {
+                queue.pop_front();
+                continue;
+            }
+            let e: &mut u64 = sequence_earnings.entry(key.clone()).or_default();
+            *e += price;
+            seen.insert(key);
+            queue.pop_front();
+        }
+    }
+    sequence_earnings
+        .iter()
+        .fold(0, |acc, (_key, &total)| std::cmp::max(acc, total))
 }
 
 #[cfg(test)]
@@ -46,6 +82,11 @@ mod tests {
 100
 2024";
 
+    const SECOND_EXAMPLE: &str = "1
+2
+3
+2024";
+
     #[test]
     fn test_part_one_as_given() {
         let result = part_one(&String::from(EXAMPLE_DATA));
@@ -54,8 +95,8 @@ mod tests {
 
     #[test]
     fn test_part_two_as_given() {
-        let result = part_two(&String::from(EXAMPLE_DATA));
-        assert_eq!(result, u64::MAX);
+        let result = part_two(&String::from(SECOND_EXAMPLE));
+        assert_eq!(result, 23);
     }
 }
 
